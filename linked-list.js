3bd5bb7097dc7991d47b7d05eaadc89bf93bf087
João Oliveira <jaoovit@gmail.com>
2024-05-08 15:54:16 +0100

    create to function to creand and add item in the list, in the start and in the end

--- linked-list.js
+++ linked-list.js
@@ -11,4 +11,40 @@ class LinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  preepend(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
+
+const list = new LinkedList();
+
+list.append(1);
+list.preepend(2);
+
+console.log(list);
