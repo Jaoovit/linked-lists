69564fbf8e5bfa6e7005536b7bb08ccc5d482717
João Oliveira <jaoovit@gmail.com>
2024-05-08 15:25:19 +0100

    create a new class to represent the full list

--- linked-list.js
+++ linked-list.js
@@ -1,5 +1,14 @@
 class Node {
   constructor(value) {
-    (this.value = value), (this.next = null);
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 }
